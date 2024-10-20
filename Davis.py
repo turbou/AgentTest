4213057925d08ca848a15bf1721fea81a0502dd9
Taka Shiozaki <taka.shiozaki@contrastsecurity.com>
2024-10-21 00:08:54 +0900

    update.

--- Davis.py
+++ Davis.py
@@ -2,7 +2,7 @@ import re
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     page.goto("http://localhost:8001/")
