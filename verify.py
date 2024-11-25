67389cb818f091076ec198c6ec2102e056ccaa1e
Taka Shiozaki <taka.shiozaki@contrastsecurity.com>
2024-11-26 00:31:50 +0900

    update.

--- verify.py
+++ verify.py
@@ -192,8 +192,6 @@ def main():
             err_msg_buffer.append('* レポートPDFには「HQLインジェクション」が含まれていません。')
 
     output_buffer = []
-    output_buffer.append('検証結果') 
-    output_buffer.append('===========================================================')
     output_buffer.append('\\n'.join(result_msg_buffer))
     output_buffer.append('-----------------------------------------------------------') 
     if len(err_msg_buffer) > 0:
@@ -201,7 +199,6 @@ def main():
         output_buffer.append('\\n'.join(err_msg_buffer))
     else:
         output_buffer.append('検証が成功しました。')
-    output_buffer.append('===========================================================')
 
     with open("output.txt", "w") as f:
         for output in output_buffer:
